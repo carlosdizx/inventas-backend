ab64c96f3f7ab763fc6a3c74cd93e04244e38036
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-13 16:29:41 -0500

    Initial commit

---
+++ src/main/java/arena/InventasApplication.java
@@ -0,0 +1,13 @@
+package arena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/arena/InventasApplicationTests.java
@@ -0,0 +1,13 @@
+package arena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
