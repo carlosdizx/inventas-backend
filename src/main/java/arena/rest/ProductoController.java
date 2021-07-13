5682bf93ad54ddddb796e0ea508c1455abd16f0d
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-13 17:55:44 -0500

    Creado archivo http para testear el rest

--- src/main/java/arena/rest/ProductoController.java
+++ src/main/java/arena/rest/ProductoController.java
@@ -15,7 +15,8 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:8080"})
+//@CrossOrigin(origins = {"http://localhost:8080","*"})
+@CrossOrigin(origins = "*")
 @RequestMapping("api/productos/")
 public class ProductoController
 {
