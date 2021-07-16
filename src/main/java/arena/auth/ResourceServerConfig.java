c3c45bed3d9d5bd15f6d46ac21dd9544f9afe14d
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-16 10:09:45 -0500

    se creo y configuro el servidor de recursos, ademas se agrego una configuracion para el servidor base de seguridad

---
+++ src/main/java/arena/auth/ResourceServerConfig.java
@@ -0,0 +1,19 @@
+package arena.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableAuthorizationServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter
+{
+    @Override
+    public void configure(HttpSecurity http) throws Exception
+    {
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clientes/all").permitAll()
+        .anyRequest().authenticated();
+    }
+}
--- src/main/java/arena/auth/SpringSecurityConfig.java
+++ src/main/java/arena/auth/SpringSecurityConfig.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class Seguridad extends WebSecurityConfigurerAdapter {
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Qualifier("UsuarioService")
     @Autowired
@@ -33,5 +35,15 @@ public class Seguridad extends WebSecurityConfigurerAdapter {
     @Bean("authenticationManager")
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception
+    {
+        http.authorizeRequests()
+        .anyRequest().authenticated()
+        .and()
+        .csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
