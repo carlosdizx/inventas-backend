7a3822f476e221c36e3db61ce5ff6eac33ff3f5d
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-16 09:19:03 -0500

    Creado la clase de autorizacion y configurada para funcionar con jwt en el endpoint, falta la config de cliente y seguridad

--- src/main/java/arena/auth/Seguridad.java
+++ src/main/java/arena/auth/Seguridad.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -26,5 +27,11 @@ public class Seguridad extends WebSecurityConfigurerAdapter {
     @Autowired
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userdetails).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    @Bean("authenticationManager")
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
